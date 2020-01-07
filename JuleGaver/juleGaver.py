5dbbc7ed913334ef7cad176b58a7338d00ca75bf
fredrikj31 <fredrik1206@gmail.com>
2020-01-07 13:47:39 +0100

    Finish Product

--- JuleGaver/juleGaver.py
+++ JuleGaver/juleGaver.py
@@ -15,14 +15,33 @@ udskriv navnene på den/dem der bliver sure, eller INGEN hvis ingen af de søske
 """
 
 def main():
-	gaver = input("Hvor mange gaver fik de Anna, Laura og Oscar >> ")
+	gaver = input()
 	gaverSplit = gaver.split()
+	StatusAnna = 0
+	StatusLaura = 0
+	StatusOscar = 0
 	AnnaAntalGaver = int(gaverSplit[0])
 	LauraAntalGaver = int(gaverSplit[1])
 	OscarAntalGaver = int(gaverSplit[2])
 
 	if AnnaAntalGaver <= 100 and LauraAntalGaver <= 100 and OscarAntalGaver <= 100:
-		print("Success")
+		#Anna bliver sur, hvis både Laura og Oscar har fået flere julegaver end hende.
+		if AnnaAntalGaver < LauraAntalGaver and AnnaAntalGaver < OscarAntalGaver:
+			StatusAnna = 1
+			print("Anna")
+
+		#Laura bliver sur hvis Anna har fået flere gaver end hende
+		if LauraAntalGaver < AnnaAntalGaver:
+			StatusLaura = 1
+			print("Laura")
+
+		#Oscar bliver sur hvis Anna eller Laura (eller begge) har fået flere gaver end ham.
+		if OscarAntalGaver < AnnaAntalGaver or OscarAntalGaver < LauraAntalGaver:
+			StatusOscar = 1
+			print("Oscar")
+	
+		if StatusAnna == 0 and StatusLaura == 0 and StatusOscar == 0:
+			print("INGEN")
 	else:
 		print("Der må maks gives 100 gaver!")
 
